31c808345eff7dd02c4257fab304d136cbbd02de
amirmub <amirmubarek01@gmail.com>
2025-03-21 09:43:25 +0300

    add carousel component and images for enhanced UI

---
+++ amazone/src/assets/images/data.jsx
@@ -0,0 +1,6 @@
+import img1 from "./10001.jpg"
+import img2 from "./10000.jpg"
+import img3 from "./1000.jpg"
+import img4 from "./10002.jpg"
+
+export const img =[img1,img2,img3,img4]
---
+++ amazone/src/components/Carousal/Carousal.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import {img} from "../../assets/images/data"
+import classes from "./Carousal.module.css"
+
+function Carousal() {
+  return (
+    <>
+       
+        <div> 
+            <Carousel
+                showThumbs={false}
+                showArrows={true}
+                showStatus={false}
+                autoPlay={true} 
+                showIndicators={false}
+                infiniteLoop={true}
+            >
+                    {
+                        img.map((imgLink)=>{
+                            return <img src={imgLink} />
+                        })
+                    }
+            </Carousel>
+            <div className={classes.shadow}>
+
+            </div>
+        </div>
+
+        
+    </>
+  )
+}
+
+export default Carousal
