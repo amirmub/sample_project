f3de83f290fc50138ed75835662602b727cf44e4
amirmub <amirmubarek01@gmail.com>
2025-03-25 17:54:55 +0300

    update ProductCard layout and styling and  add 'add to cart' button

--- amazone/src/components/Product/ProductCard.jsx
+++ amazone/src/components/Product/ProductCard.jsx
@@ -12,12 +12,15 @@ function ProductCard({result}) {
         <div>
             <div className={classes.title}>{result.title}</div>
             <div className={classes.rating}> 
-                <Rating name="half-rating" defaultValue={result.rating.rate} precision={0.2} />
+                <Rating name="half-rating" defaultValue={result.rating.rate} precision={0.2}  readOnly/>
                 <p>{result.rating.count}</p>
             </div>
             <div className={classes.price}>
                {`$${result.price}`}
             </div>
+        </div>
+        <div className={classes.cart__btn}>
+           <button>add to cart</button>
         </div>
       </div>
       
