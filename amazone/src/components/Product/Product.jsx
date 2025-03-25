35df6c3d8f25dd73d66f9c6768d0c58270c6015e
amirmub <amirmubarek01@gmail.com>
2025-03-25 11:23:21 +0300

    add a product component

--- amazone/src/components/Product/Product.jsx
+++ amazone/src/components/Product/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from 'react'
 import axios from "axios"
-import ProductCard from './ProductCard';
+import ProductCard from '../ProductCard';
 
 function Product() {
     const [products,setProducts] = useState([]);
